fad3c312d2e3e89f4418ea4b736c73d79bd97cc1
trcasper <trcasper90@gmail.com>
2019-10-18 11:25:51 -0600

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Dashboard from './Components/Dashboard/Dashboard';
+import House from './Components/House/House';
+import Header from './Components/Header/Header'
+import Wizard from './Components/Wizard/Wizard'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Dashboard />
+      <House />
+      <Wizard />
     </div>
   );
 }
---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import House from '../House/House'
+
+export default function Dashboard(){
+    return(
+        <div>
+            Dashboard
+            <House />
+        </div>
+        
+        
+
+        
+    )
+}
